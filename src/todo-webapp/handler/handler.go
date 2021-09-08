2a616151d5063e21409afcc86af3be7ed72ec8a3
naptimes <alibasaeb@gmail.com>
2021-09-08 21:47:33 +0700

    fixing register, login api

--- src/todo-webapp/handler/handler.go
+++ src/todo-webapp/handler/handler.go
@@ -3,14 +3,29 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var dns = "todoAdmin:IniPassword456$@tcp(127.0.0.1:3306)/todowebapp?charset=utf8mb4&parseTime=True&loc=Local"
+
+var DB, Err = gorm.Open(mysql.Open(dns), &gorm.Config{})
+
+// type User struct {
+// 	FirstName     string   `json:"fname"`
+// 	Lastname      string   `json:"lname"`
+// 	LoginAttribut LoginAtt `json:"login-att"`
+// }
+
 type User struct {
-	FirstName     string   `json:"fname"`
-	Lastname      string   `json:"lname"`
-	LoginAttribut LoginAtt `json:"login-att"`
+	Fname    string `json:"fname"`
+	Lname    string `json:"lname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type LoginAtt struct {
@@ -18,31 +33,68 @@ type LoginAtt struct {
 	Password string `json:"password"`
 }
 
+type Result struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
+
 func Health(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
+// FIX
 func Register(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Ini route register"))
+	w.Header().Set("content-type", "application/json")
+
+	body, _ := ioutil.ReadAll(r.Body)
+
+	var user User
+	json.Unmarshal(body, &user)
+
+	var res Result
+
+	DB.Create(&user)
+	if Err != nil {
+		res = Result{Status: 406, Message: http.StatusText(http.StatusNotAcceptable)}
+		w.WriteHeader(http.StatusNotAcceptable)
+	} else {
+		res = Result{Status: 201, Message: http.StatusText(http.StatusAccepted)}
+		w.WriteHeader(http.StatusAccepted)
+	}
+
+	result, _ := json.Marshal(res)
+
+	w.Write(result)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Ini route login"))
 }
 
+// FIX
 func LoginPress(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 
-	loginTest := LoginAtt{
-		Email: "mockb@mal.com", Password: "IniMockPassword123@"}
+	body, _ := ioutil.ReadAll(r.Body)
 
-	// for checking user credentail (temporary)
-	if loginTest.Email != "mockb@mail.com" || loginTest.Password != "IniMockPassword123@" {
-		http.Error(w, http.StatusText(http.StatusNotAcceptable), http.StatusNotAcceptable)
-		w.Write([]byte("terjadi error"))
-		return
+	var user User
+	var catch User
+	var res Result
+
+	json.Unmarshal(body, &catch)
+
+	DB.First(&user, catch)
+	if user.Email == "" {
+		res = Result{Status: 406, Message: http.StatusText(http.StatusNotAcceptable)}
+		w.WriteHeader(http.StatusNotAcceptable)
+	} else {
+		res = Result{Status: 200, Message: http.StatusText(http.StatusOK)}
+		w.WriteHeader(http.StatusOK)
 	}
-	json.NewEncoder(w).Encode(loginTest)
+
+	result, _ := json.Marshal(res)
+
+	w.Write(result)
 }
 
 func ForgotPassword(w http.ResponseWriter, r *http.Request) {
